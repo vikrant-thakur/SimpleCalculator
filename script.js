80d363a0da86c57d21d29e75598e3d45582d5cba
vikrant-thakur <vikrant.thakur@crispanalytics.com>
2018-06-21 17:22:58 +0530

    Evaluate expression manually applying bodmas rule

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function displayOperator(val){
+function displayOperator(val){                       					  // Covers cases of multiple operator pressing
 	text = document.getElementById("disp").value
 	if((text[text.length - 1]==".") && val=="."){
 		document.getElementById("disp").value = document.getElementById("disp").value;
@@ -14,7 +14,41 @@ function displayOperator(val){
 	}
 }
 
-function backspace(){
+function backspace(){                               					  // One backspace at a time 
 	text = document.getElementById("disp").value
 	document.getElementById("disp").value = text.substr(0,text.length-1);
 }
+
+
+function express() {													  // Evaluates the expression
+    var str = document.getElementById("disp").value; 
+    var res = str.match(/[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)/g);          // REGEX to match signed floats
+    for (i=0;i<res.length;i++){
+    	str = str.replace(res[i],"N");
+    }
+    var ops = str.split("N")                                              // Operators array
+    var i=-1;var n=-1;
+    while (n < ops.length-1){
+    	i+=1;n+=1;
+        if (ops[n]=="*"){
+        	var store = parseFloat(res[i-1])*parseFloat(res[i]);
+            res.splice(i-1,1); res.splice(i-1,1);
+            res.splice(i-1,0,store.toString());
+            i-=1;
+        }
+        if (ops[n]=="/"){
+        	var store = parseFloat(res[i-1])/parseFloat(res[i]);
+            res.splice(i-1,1); res.splice(i-1,1);
+            res.splice(i-1,0,store.toString());
+            i-=1;
+        }
+    }
+    var ans = []; var result=0;
+    for (j=0;j<res.length;j++){											 // Parsing to floats
+    	ans.push(parseFloat(res[j]));
+    }
+    for (k=0;k<ans.length;k++){											 // Adding final results
+    	result+=ans[k];
+    }
+    return result.toFixed(8);
+}    
